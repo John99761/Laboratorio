0a7b62bdd60b05b691925e9c024ee9c249c3d1f2
John99761 <113964308+John99761@users.noreply.github.com>
2024-05-20 19:12:57 -0700

    Laboratorio

---
+++ ejercicio2.py
@@ -0,0 +1,13 @@
+
+a = 4  
+b = 6  
+
+# Calcular el perímetro del rectángulo
+perimetro = 2 * (a + b)
+
+# Calcular el área del rectángulo
+area = a * b
+
+# Mostrar los resultados
+print(f"El perímetro del rectángulo es: {perimetro} metros")
+print(f"El área del rectángulo es: {area} metros cuadrados")
---
+++ ejercicio6.py
@@ -0,0 +1,9 @@
+# Solicitar al usuario dos números
+dividendo = float(input("Introduce el dividendo: "))
+divisor = float(input("Introduce el divisor: "))
+
+# Calcular el residuo de la división
+residuo = dividendo % divisor
+
+# Mostrar el residuo de la división
+print(f"El residuo de la división entre {dividendo} y {divisor} es: {residuo}")
