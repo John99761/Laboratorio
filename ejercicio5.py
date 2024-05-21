68a22e49b9cb7e5a54b3498d8f14b0fcb39b88e4
John99761 <113964308+John99761@users.noreply.github.com>
2024-05-20 19:14:37 -0700

    ejercicio5.py

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,13 +1,13 @@
-# Solicitar al usuario el valor de los dos lados del rectángulo
+
 lado1 = float(input("Introduce la longitud del primer lado del rectángulo: "))
 lado2 = float(input("Introduce la longitud del segundo lado del rectángulo: "))
 
-# Calcular el perímetro del rectángulo
+
 perimetro = 2 * (lado1 + lado2)
 
-# Calcular el área del rectángulo
+
 area = lado1 * lado2
 
-# Mostrar los resultados
+
 print(f"El perímetro del rectángulo es: {perimetro}")
 print(f"El área del rectángulo es: {area}")
