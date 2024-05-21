cce0e29310fafae8a93616b01dd293bc6018f98e
John99761 <113964308+John99761@users.noreply.github.com>
2024-05-20 19:15:36 -0700

    ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -2,12 +2,12 @@
 a = 4  
 b = 6  
 
-# Calcular el perímetro del rectángulo
+
 perimetro = 2 * (a + b)
 
-# Calcular el área del rectángulo
+
 area = a * b
 
-# Mostrar los resultados
+
 print(f"El perímetro del rectángulo es: {perimetro} metros")
 print(f"El área del rectángulo es: {area} metros cuadrados")
