f1a4ac43a3244444aa1d9a9a1d222f970f0080c7
John99761 <113964308+John99761@users.noreply.github.com>
2024-05-20 19:15:16 -0700

    ejercicio7.py

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,9 +1,9 @@
-# Solicitar al usuario el valor de la base y la altura del triángulo
+
 base = float(input("Introduce la longitud de la base del triángulo: "))
 altura = float(input("Introduce la altura del triángulo: "))
 
-# Calcular el área del triángulo
+
 area = (base * altura) / 2
 
-# Mostrar el área del triángulo
+
 print(f"El área del triángulo con base {base} y altura {altura} es: {area}")
